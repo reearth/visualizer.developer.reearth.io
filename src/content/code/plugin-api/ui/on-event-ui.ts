96d29e4fdbbdcc5638128e1731f76a6af2e8fae1
OpeDada <opedada@gmail.com>
2024-10-24 17:38:50 +0900

    write ui first draft

--- src/content/code/plugin-api/ui/on-event-ui.ts
+++ src/content/code/plugin-api/ui/on-event-ui.ts
@@ -1,12 +1,12 @@
 // @ts-nocheck
 
 // Register a listener for the 'update' event
-reearth.camera.on("update", () => {
+reearth.ui.on("update", () => {
   console.log("UI was updated.");
 });
 
 // Register a one-time listener for the 'close' event
-reearth.camera.on(
+reearth.ui.on(
   "close",
   () => {
     console.log("UI was closed.");
